54c9a607af1968ca24ab82365f9dfc90c8097ee2
DarkOfyther <l.goncalvess5002@gmail.com>
2024-09-12 10:57:23 -0300

    Foi relacionado a lista de contatos ao nosso app.

--- contatinhos/src/app/home/index.tsx
+++ contatinhos/src/app/home/index.tsx
@@ -1,16 +1,35 @@
-import { View } from "react-native"
+import { View, Alert } from "react-native"
 import { Feather } from '@expo/vector-icons'
-import * as Contacts from 'expo-contacts'
+import { useState, useEffect } from 'react'
 import { styles } from './styles'
+import * as Contacts from 'expo-contacts'
 import { Input } from "@/app/components/input"
 import { theme } from "@/theme"
-import { useState } from "react"
 import { Contact } from "@/app/components/contact"
 
 export function Home(){
     const [name, setName] = useState("")
 
+
+
+    async function fetchContacts() {
+        try {
+            const { status } = await Contacts.requestPermissionsAsync()
+            if (status === Contacts.PermissionStatus.GRANTED){
+                const { data } = await Contacts.getContactsAsync()
+                console.log(data)
+            }
+        } catch(error){
+            console.log(error)
+            Alert.alert("Contatos", "Não foi possivel carregar os contatos...")        
+        }
+    } 
+    useEffect(() => {
+        fetchContacts()
+    },[])
+    
     return (
+
         <View style={styles.container}>
             <View style={styles.header}>
                 <Input style={styles.input}>
